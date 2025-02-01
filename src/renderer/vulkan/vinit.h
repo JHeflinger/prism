65e4a432104896a5234dfcb3b6b2c0224563c7ba
Jason Heflinger <jasonheflinger@gmail.com>
2025-02-01 15:27:14 -0500

    moving

--- src/renderer/vulkan/vinit.h
+++ src/renderer/vulkan/vinit.h
@@ -4,6 +4,26 @@
 #include "data/config.h"
 #include "renderer/vulkan/vstructs.h"
 
+BOOL VINIT_Queue(VkQueue* queue);
+
+BOOL VINIT_Commands(VulkanCommands* commands);
+
+BOOL VINIT_Syncro(VulkanSyncro* syncro);
+
+BOOL VINIT_UniformBuffers(UBOArray* ubos);
+
+BOOL VINIT_Descriptors(VulkanDescriptors* descriptors);
+
+BOOL VINIT_Attachments(VulkanAttachments* attachments);
+
+BOOL VINIT_Framebuffers(VkFramebuffer* framebuffer);
+
+BOOL VINIT_RenderData(VulkanRenderData* renderdata);
+
+BOOL VINIT_RenderPass(VkRenderPass* renderpass);
+
+BOOL VINIT_Pipeline(VulkanPipeline* pipeline);
+
 BOOL VINIT_Scheduler(VulkanScheduler* scheduler);
 
 BOOL VINIT_Bridge(VulkanDataBuffer* bridge);
@@ -23,5 +43,7 @@ BOOL VINIT_Metadata(VulkanMetadata* metadata);
 BOOL VINIT_Core(VulkanCore* core);
 
 BOOL VINIT_Vulkan(VulkanObject* vulkan);
+
+BOOL VINIT_RendererReference(Renderer* renderer);
 
 #endif
