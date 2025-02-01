e94594907237199b3963ff852d59d551d826e89b
Jason Heflinger <jasonheflinger@gmail.com>
2025-02-01 15:28:27 -0500

    moving back

--- src/renderer/vulkan/vinit.c
+++ src/renderer/vulkan/vinit.c
@@ -1,1 +1,85 @@
 #include "vinit.h"
+
+BOOL VINIT_Queue(VkQueue* queue) {
+    return FALSE;
+}
+
+BOOL VINIT_Commands(VulkanCommands* commands) {
+    return FALSE;
+}
+
+BOOL VINIT_Syncro(VulkanSyncro* syncro) {
+    return FALSE;
+}
+
+BOOL VINIT_UniformBuffers(UBOArray* ubos) {
+    return FALSE;
+}
+
+BOOL VINIT_Descriptors(VulkanDescriptors* descriptors) {
+    return FALSE;
+}
+
+BOOL VINIT_Attachments(VulkanAttachments* attachments) {
+    return FALSE;
+}
+
+BOOL VINIT_Framebuffers(VkFramebuffer* framebuffer) {
+    return FALSE;
+}
+
+BOOL VINIT_RenderData(VulkanRenderData* renderdata) {
+    return FALSE;
+}
+
+BOOL VINIT_RenderPass(VkRenderPass* renderpass) {
+    return FALSE;
+}
+
+BOOL VINIT_Pipeline(VulkanPipeline* pipeline) {
+    return FALSE;
+}
+
+BOOL VINIT_Scheduler(VulkanScheduler* scheduler) {
+    return FALSE;
+}
+
+BOOL VINIT_Bridge(VulkanDataBuffer* bridge) {
+    return FALSE;
+}
+
+BOOL VINIT_RenderContext(VulkanRenderContext* context) {
+    return FALSE;
+}
+
+BOOL VINIT_General(VulkanGeneral* general) {
+    return FALSE;
+}
+
+BOOL VINIT_Vertices(VulkanDataBuffer* vertices) {
+    return FALSE;
+}
+
+BOOL VINIT_Indices(VulkanDataBuffer* indices) {
+    return FALSE;
+}
+
+BOOL VINIT_Geometry(VulkanGeometry* geometry) {
+    return FALSE;
+}
+
+BOOL VINIT_Metadata(VulkanMetadata* metadata) {
+    return FALSE;
+}
+
+BOOL VINIT_Core(VulkanCore* core) {
+    return FALSE;
+}
+
+BOOL VINIT_Vulkan(VulkanObject* vulkan) {
+    return FALSE;
+}
+
+BOOL VINIT_RendererReference(Renderer* renderer) {
+    return FALSE;
+}
