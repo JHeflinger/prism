8455acdc48699880f16bbeccb443f8ef96345f13
Jason Heflinger <jasonheflinger@gmail.com>
2025-04-07 12:29:33 -0400

    prettied up build script

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: total timing
+set "t_startTime=%time: =0%"
+
 :: audit codebase
 python scripts/help.py audit
 
@@ -63,23 +66,25 @@ set SHADERS_UP_TO_DATE="true"
 for /r %SHADERS_DIR% %%f in (*.vert *.frag *.comp) do (
     if NOT exist "build/cache/shaders/%%~nxf" (
         set SHADERS_UP_TO_DATE="false"
-        echo - [%%~nxf]
+        echo - [%%~nxf] [33m^(compiling...^)[0m
         "platform/windows/glslc/glslc.exe" %%f -o "build/shaders/%%~nxf.spv"
         if !ERRORLEVEL! NEQ 0 (
-            echo Building vertex [31mFailed[0m with error code !ERRORLEVEL!
+            echo Building shader [31mFailed[0m with error code !ERRORLEVEL!
             exit /b !ERRORLEVEL!
         )
+        echo [1A[0K- [%%~nxf] [32mOK[0m
         copy /y %%f "build/cache/shaders/%%~nxf" >nul
     ) else (
         fc %%f "build/cache/shaders/%%~nxf" >nul
         if !ERRORLEVEL! NEQ 0 (
             set SHADERS_UP_TO_DATE="false"
-            echo - [%%~nxf]
+            echo - [%%~nxf] [33m^(compiling...^)[0m
             "platform/windows/glslc/glslc.exe" %%f -o "build/shaders/%%~nxf.spv"
             if !ERRORLEVEL! NEQ 0 (
-                echo Building vertex [31mFailed[0m with error code !ERRORLEVEL!
+                echo Building shader [31mFailed[0m with error code !ERRORLEVEL!
                 exit /b !ERRORLEVEL!
             )
+            echo [1A[0K- [%%~nxf] [32mOK[0m
             copy /y %%f "build/cache/shaders/%%~nxf" >nul
         )
     )
@@ -89,7 +94,7 @@ set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,
 set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
 set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
 if %SHADERS_UP_TO_DATE%=="true" (
-    echo Shaders are currently [32mup to date[0m
+    echo [1A[0KShaders are currently [32mup to date[0m
 ) else (
     echo [32mFinished[0m building shaders in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1%
 )
@@ -147,6 +152,11 @@ if defined SOURCES (
             echo Building vendors [31mFailed[0m with error code !ERRORLEVEL!
             exit /b !ERRORLEVEL!
         )
+        set "endTime=%time: =0%"
+        set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
+        set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
+        set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
+        echo [32mFinished[0m compiling vendor in !hh:~1!!time:~2,1!!mm:~1!!time:~2,1!!ss:~1!!time:~8,1!!cc:~1!
     )
     set OBJECTS=!OBJECTS! build/vendor/vendor.o
 )
@@ -155,29 +165,32 @@ if defined SOURCES (
 echo Compiling sources...
 set SOURCES_UP_TO_DATE="true"
 set FOUND_MAIN="false"
+set "startTime=%time: =0%"
 for /r %SRC_DIR% %%f in (*.c) do (
     set SUBPATH=%%f
     set REL=!SUBPATH:%CD%\%SRC_DIR%=!
     if "%%~nxf" NEQ "main.c" (
         if NOT exist "build\cache\src!REL!.o" (
             set SOURCES_UP_TO_DATE="false"
-            echo - [%%~nxf]
+            echo - [%%~nxf] [33m^(compiling...^)[0m
             gcc -Wall -Wextra -Wno-unused-parameter -c %%f%INCLUDES%%LIBS%%LINKS% -o build\cache\src!REL!.o
             if !ERRORLEVEL! NEQ 0 (
                 echo Building source "%%~nxf" [31mFailed[0m with error code !ERRORLEVEL!
                 exit /b !ERRORLEVEL!
             )
+            echo [1A[0K- [%%~nxf] [32mOK[0m
             copy /y %%f build\cache\src!REL! >nul
         ) else (
             fc %%f "build\cache\src!REL!" >nul
             if !ERRORLEVEL! NEQ 0 (
                 set SOURCES_UP_TO_DATE="false"
-                echo - [%%~nxf]
+                echo - [%%~nxf] [33m^(compiling...^)[0m
                 gcc -Wall -Wextra -Wno-unused-parameter -c %%f%INCLUDES%%LIBS%%LINKS% -o build\cache\src!REL!.o
                 if !ERRORLEVEL! NEQ 0 (
                     echo Building source "%%~nxf" [31mFailed[0m with error code !ERRORLEVEL!
                     exit /b !ERRORLEVEL!
                 )
+                echo [1A[0K- [%%~nxf] [32mOK[0m
                 copy /y %%f build\cache\src!REL! >nul
             )
         )
@@ -187,7 +200,13 @@ for /r %SRC_DIR% %%f in (*.c) do (
     )
 )
 if %SOURCES_UP_TO_DATE%=="true" (
-    echo Sources are currently [32mup to date[0m
+    echo [1A[0KSources are currently [32mup to date[0m
+) else (
+    set "endTime=%time: =0%"
+    set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
+    set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
+    set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
+    echo [32mFinished[0m compiling sources in !hh:~1!!time:~2,1!!mm:~1!!time:~2,1!!ss:~1!!time:~8,1!!cc:~1!
 )
 if %FOUND_MAIN%=="false" (
     echo [31mError[0m: unable to compile without a detected "src/main.c" file!
@@ -196,6 +215,7 @@ if %FOUND_MAIN%=="false" (
 
 :: compile executable
 echo Building executable...
+set "startTime=%time: =0%"
 gcc -Wall -Wextra -Wno-unused-parameter src/main.c%OBJECTS%%INCLUDES%%LIBS%%LINKS% -o build/prism.exe %PROD%
 if !ERRORLEVEL! NEQ 0 (
     echo Build [31mFailed[0m with error code !ERRORLEVEL!
@@ -206,3 +226,8 @@ set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,
 set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
 set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
 echo [32mFinished[0m building executable in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1%
+set "endTime=%time: =0%"
+set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!t_startTime:%time:~8,1%=%%100)*100+1!"
+set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
+set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
+echo [32mFinished[0m total build in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1%
