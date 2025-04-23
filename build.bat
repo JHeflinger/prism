5429e4ebff435dc107631abd8f0fb1e52a81d573
Jason Heflinger <jasonheflinger@gmail.com>
2025-04-22 20:47:46 -0400

    updated scripts to windows versions

--- build.bat
+++ build.bat
@@ -1,33 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: total timing
-set "t_startTime=%time: =0%"
-
-:: audit codebase
-python scripts/help.py audit
-
-:: initialize vars for building
-set SRC_DIR=src
-set INCLUDES=
-set SOURCES=
-set OBJECTS=
-set LIBS=
-set LINKS=
-
-:: production build flags
-set PROD=
-if "%1"=="prod" (
-    echo Optimizing for production build...
-    set PROD=-O3 -DPROD_BUILD
-)
-
-:: create build directory if it does not exist
+:: create all build directories if it does not exist
 if NOT exist "build\" (
     mkdir build
 )
-
-:: create all build directories if it does not exist
 cd build
 if NOT exist "shaders\" (
     mkdir shaders
@@ -35,28 +12,12 @@ if NOT exist "shaders\" (
 if NOT exist "cache\" (
     mkdir cache
 )
-if NOT exist "vendor\" (
-    mkdir vendor
-)
 cd cache
 if NOT exist "shaders\" (
     mkdir shaders
 )
-if NOT exist "src\" (
-    mkdir src
-)
 cd ..
 cd ..
-
-:: set up cache folders
-for /d /r %SRC_DIR% %%D in (*) do (
-    set SUBPATH=%%D
-    set REL=!SUBPATH:%CD%\%SRC_DIR%=!
-    set DESTDIR=build\cache\src!REL!
-    if not exist "!DESTDIR!" (
-        mkdir !DESTDIR!
-    )
-)
 
 :: compile shaders
 echo Building shaders...
@@ -99,137 +60,32 @@ if %SHADERS_UP_TO_DATE%=="true" (
     echo [32mFinished[0m building shaders in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1%
 )
 
-:: get includes
-for /r %SRC_DIR% %%d in (.) do (
-    set INCLUDES=!INCLUDES! -I"%%d"
+:: download builder
+if NOT exist "build\tiny_windows.exe" (
+    echo Downloading tiny builder...
+    cd build
+    PowerShell -Command "Invoke-WebRequest -Uri 'https://github.com/JHeflinger/tiny/raw/refs/heads/main/bin/tiny_windows.exe' -OutFile 'tiny_windows.exe'"
+    cd ..
 )
-
-:: add raylib vendor
-set INCLUDES=!INCLUDES! -I"vendor/raylib/include"
-set LINKS=!LINKS! -l:win_x64_libraylib.a
-set LIBS=!LIBS! -L"vendor/raylib/lib"
-
-:: add stb_image vendor
-set INCLUDES=!INCLUDES! -I"vendor/stb_image/include"
-
-:: add vulkan vendor
-set INCLUDES=!INCLUDES! -I"platform/windows/vulkan/include"
-set LIBS=!LIBS! -L"platform/windows/vulkan/libs"
-set LINKS=!LINKS! -lvulkan-1
-
-:: add glfw vendor
-set INCLUDES=!INCLUDES! -I"platform/windows/GLFW/include"
-set LIBS=!LIBS! -L"platform/windows/GLFW"
-set LINKS=!LINKS! -lglfw3
-set LINKS=!LINKS! -lshell32
-set LINKS=!LINKS! -luser32
-set LINKS=!LINKS! -lopengl32
-set LINKS=!LINKS! -lgdi32
-set LINKS=!LINKS! -lwinmm
-set LINKS=!LINKS! -lwinpthread
-set LINKS=!LINKS! -lws2_32
-
-:: add cglm vendor
-set INCLUDES=!INCLUDES! -I"vendor/cglm/include"
-
-:: EasyC vendor
-set INCLUDES=!INCLUDES! -I"vendor/EasyC/include"
-for /r "vendor/EasyC/include" %%f in (*.c) do (
-	set SOURCES=!SOURCES! %%f
-)
-
-:: compile vendor
-set "startTime=%time: =0%"
-if defined SOURCES (
-    if NOT exist "build\vendor\vendor.o" (
-        echo Compiling vendors...
-		type nul > "build/vendor/merged_vendors.c"
-		for %%f in (!SOURCES!) do (
-			set "fp=%%f"
-			set "fp=!fp:\=/!"
-			echo #include "../../!fp!" >> "build/vendor/merged_vendors.c"
-		)
-        gcc -Wall -Wextra -Wno-unused-parameter -c build/vendor/merged_vendors.c%INCLUDES%%LIBS%%LINKS% -o build/vendor/vendor.o %PROD%
-        if !ERRORLEVEL! NEQ 0 (
-            echo Building vendors [31mFailed[0m with error code !ERRORLEVEL!
-            exit /b !ERRORLEVEL!
-        )
-        set "endTime=%time: =0%"
-        set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
-        set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
-        set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
-        echo [32mFinished[0m compiling vendors in !hh:~1!!time:~2,1!!mm:~1!!time:~2,1!!ss:~1!!time:~8,1!!cc:~1!
+if "%1"=="-u" (
+    if exist "build\tiny_windows.exe" (
+        del \f \q build\tiny_windows.exe
     )
-    set OBJECTS=!OBJECTS! build/vendor/vendor.o
+    echo Updating tiny builder...
+    cd build
+    PowerShell -Command "Invoke-WebRequest -Uri 'https://github.com/JHeflinger/tiny/raw/refs/heads/main/bin/tiny_windows.exe' -OutFile 'tiny_windows.exe'"
+    cd ..
 )
 
-:: compile obj files
-echo Compiling sources...
-set SOURCES_UP_TO_DATE="true"
-set FOUND_MAIN="false"
-set "startTime=%time: =0%"
-for /r %SRC_DIR% %%f in (*.c) do (
-    set SUBPATH=%%f
-    set REL=!SUBPATH:%CD%\%SRC_DIR%=!
-    if "%%~nxf" NEQ "main.c" (
-        if NOT exist "build\cache\src!REL!.o" (
-            set SOURCES_UP_TO_DATE="false"
-            echo - [%%~nxf] [33m^(compiling...^)[0m
-            gcc -Wall -Wextra -Wno-unused-parameter -c %%f%INCLUDES%%LIBS%%LINKS% -o build\cache\src!REL!.o
-            if !ERRORLEVEL! NEQ 0 (
-                echo Building source "%%~nxf" [31mFailed[0m with error code !ERRORLEVEL!
-                exit /b !ERRORLEVEL!
-            )
-            echo [1A[0K- [%%~nxf] [32mOK[0m
-            copy /y %%f build\cache\src!REL! >nul
-        ) else (
-            fc %%f "build\cache\src!REL!" >nul
-            if !ERRORLEVEL! NEQ 0 (
-                set SOURCES_UP_TO_DATE="false"
-                echo - [%%~nxf] [33m^(compiling...^)[0m
-                gcc -Wall -Wextra -Wno-unused-parameter -c %%f%INCLUDES%%LIBS%%LINKS% -o build\cache\src!REL!.o
-                if !ERRORLEVEL! NEQ 0 (
-                    echo Building source "%%~nxf" [31mFailed[0m with error code !ERRORLEVEL!
-                    exit /b !ERRORLEVEL!
-                )
-                echo [1A[0K- [%%~nxf] [32mOK[0m
-                copy /y %%f build\cache\src!REL! >nul
-            )
-        )
-        set OBJECTS=!OBJECTS! build\cache\src!REL!.o
-    ) else (
-        set FOUND_MAIN="true"
-    )
+:: run builder
+set PROD=
+if "%1"=="-p" (
+    set PROD=-prod
 )
-if %SOURCES_UP_TO_DATE%=="true" (
-    echo [1A[0KSources are currently [32mup to date[0m
-) else (
-    set "endTime=%time: =0%"
-    set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
-    set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
-    set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
-    echo [32mFinished[0m compiling sources in !hh:~1!!time:~2,1!!mm:~1!!time:~2,1!!ss:~1!!time:~8,1!!cc:~1!
+if "%2"=="-p" (
+    set PROD=-prod
 )
-if %FOUND_MAIN%=="false" (
-    echo [31mError[0m: unable to compile without a detected "src/main.c" file!
-    exit /b !ERRORLEVEL!
-)
-
-:: compile executable
-echo Building executable...
-set "startTime=%time: =0%"
-gcc -Wall -Wextra -Wno-unused-parameter src/main.c%OBJECTS%%INCLUDES%%LIBS%%LINKS% -o build/prism.exe %PROD%
+"./build/tiny_windows.exe" -a %PROD%
 if !ERRORLEVEL! NEQ 0 (
-    echo Build [31mFailed[0m with error code !ERRORLEVEL!
     exit /b !ERRORLEVEL!
 )
-set "endTime=%time: =0%"
-set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
-set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
-set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
-echo [32mFinished[0m building executable in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1%
-set "endTime=%time: =0%"
-set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!t_startTime:%time:~8,1%=%%100)*100+1!"
-set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
-set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
-echo [32mFinished[0m total build in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1%
--- clean.bat
+++ clean.bat
@@ -1,4 +1,13 @@
 @echo off
-if exist "build\" (
-    rmdir /s /q "build\"
+if exist "build\cache" (
+    rmdir /s /q "build\cache"
+)
+if exist "build\vendor" (
+    rmdir /s /q "build\vendor"
+)
+if exist "build\shaders" (
+    rmdir /s /q "build\shaders"
+)
+if exist "build\bin.exe" (
+    del /f /q "build\bin.exe"
 )
