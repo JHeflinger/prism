9608e3f92352093230abeff4538682706bfd0ddb
Jason Heflinger <jasonheflinger@gmail.com>
2026-09-08 23:20:08 -0400

    removed dice extension

--- extensions/dice/src/aseprite/parser.c
+++
@@ -1,2 +0,0 @@
-#define CUTE_ASEPRITE_IMPLEMENTATION
-#include "parser.h"
--- extensions/dice/src/extender.c
+++
@@ -1,6 +0,0 @@
-#include <core/extender.h>
-#include "dice.h"
-
-void ExtendPanelCreation(UI* ui) {
-    ARRLIST_Panel_add(&(((UI*)(((UI*)ui->right)->right))->panels), GenerateDicePanel());
-}
